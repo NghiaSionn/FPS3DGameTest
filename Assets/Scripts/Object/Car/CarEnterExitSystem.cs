55618690b16ec44774f227cb076ce34a25dd6729
NghiaSion <ptcn0120223@gmail.com>
2024-06-06 19:06:34 +0700

    master4

---
+++ Assets/Scripts/Object/Car/CarEnterExitSystem.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class CarEnterExitSystem : MonoBehaviour
+{
+
+    public MonoBehaviour CarController;
+    public Transform Car;
+    public Transform Player;
+
+    
+    public GameObject PlayerCam;
+    public GameObject CarCam;
+
+    public GameObject DriveUi;
+    public GameObject playerPosition;
+    public GameObject middlePoint;
+
+    bool Candrive;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CarController.enabled = false;
+        DriveUi.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F) && Candrive)  
+        {
+
+            CarController.enabled = true; 
+            
+
+            DriveUi.gameObject.SetActive(false);
+            playerPosition.gameObject.SetActive(false);
+            middlePoint.gameObject.SetActive(false);
+
+
+            // Here we parent Car with player
+            Player.transform.SetParent(Car);
+            Player.gameObject.SetActive(false);
+           
+
+            // Camera
+            PlayerCam.gameObject.SetActive(false);
+            CarCam.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CarController.enabled = false; 
+
+
+            playerPosition.gameObject.SetActive(true);
+            middlePoint.gameObject.SetActive(false);
+
+
+            // Here We Unparent the Player with Car
+            Player.transform.SetParent(null);
+            Player.gameObject.SetActive(true);
+
+           
+
+            PlayerCam.gameObject.SetActive(true);
+            CarCam.gameObject.SetActive(false);
+        }
+    }
+
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            DriveUi.gameObject.SetActive(true);
+            Candrive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            DriveUi.gameObject.SetActive(false);
+            Candrive = false;
+        }
+    }
+}
