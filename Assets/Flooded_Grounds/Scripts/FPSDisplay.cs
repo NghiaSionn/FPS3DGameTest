f26ef225c70d15ec95bfdc09dbe6a52ac8ad9bb2
Phạm Trần Chung Nghĩa <ptcn0120223@gmail.com>
2024-06-30 02:51:01 +0700

    Commit30

--- Assets/Flooded_Grounds/Scripts/FPSDisplay.cs
+++ Assets/Flooded_Grounds/Scripts/FPSDisplay.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
+using TMPro;
 
 public class FPSDisplay : MonoBehaviour
 {
-	float deltaTime = 0.0f;
-	
-	void Update()
-	{
-		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-	}
-	
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-		
-		GUIStyle style = new GUIStyle();
-		
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (1, 1, 1, 1.0f);
-		float msec = deltaTime * 1000.0f;
-		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
-	}
+    public TextMeshProUGUI fpstext;
+    private float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        UpdateFPSDisplay();
+    }
+
+    void UpdateFPSDisplay()
+    {
+        if (fpstext != null)
+        {
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            fpstext.text = text;
+        }
+    }
 }
